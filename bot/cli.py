de0d47f66aedb98b1b391e19d5d931178491bd35
narumi <toucans-cutouts0f@icloud.com>
2024-09-28 01:14:06 +0800

    remove redis cache

--- bot/cli.py
+++ bot/cli.py
@@ -2,10 +2,10 @@ from dotenv import find_dotenv
 from dotenv import load_dotenv
 
 from .bot import Bot
-from .utils import set_llm_cache_from_env
+from .utils import set_sqlite_llm_cache
 
 
 def main():
     load_dotenv(find_dotenv(raise_error_if_not_found=True, usecwd=True))
-    set_llm_cache_from_env()
+    set_sqlite_llm_cache()
     Bot.from_env()
--- bot/utils.py
+++ bot/utils.py
@@ -1,14 +1,13 @@
 import os
 import re
 import tempfile
+from pathlib import Path
 
 import httpx
 from langchain.globals import set_llm_cache
-from langchain_community.cache import BaseCache
 from langchain_community.cache import SQLiteCache
 from langchain_community.document_loaders.html_bs import BSHTMLLoader
 from langchain_community.document_loaders.pdf import PyPDFLoader
-from langchain_redis import RedisCache
 from loguru import logger
 
 DEFAULT_HEADERS = {
@@ -62,14 +61,10 @@ def load_url(url: str) -> str:
     return load_html(f)
 
 
-def set_llm_cache_from_env() -> None:
-    cache: BaseCache | None = None
+def set_sqlite_llm_cache() -> None:
+    database_path = Path.home() / ".cache" / ".langchain.db"
+    logger.info("Using SQLite cache: {}", database_path)
 
-    redis_url = os.getenv("REDIS_URL")
-    if redis_url:
-        logger.info(f"Using Redis cache at {redis_url}")
-        cache = RedisCache(redis_url, prefix="langchain")
-    else:
-        cache = SQLiteCache()
+    cache = SQLiteCache(database_path.as_posix())
 
     set_llm_cache(cache)
