ac549c62af836a3350f0840eb66e65a0f224d9d7
narumi <toucans-cutouts0f@icloud.com>
2024-09-28 17:56:58 +0800

    use filter and show chat id

--- bot/bot.py
+++ bot/bot.py
@@ -7,6 +7,7 @@ from telegram import Update
 from telegram.ext import Application
 from telegram.ext import CommandHandler
 from telegram.ext import ContextTypes
+from telegram.ext import filters
 
 from .summary import summarize
 from .translate import translate
@@ -24,8 +25,9 @@ class Bot:
 
         # Create the application and add the command handler
         self.app = Application.builder().token(token).build()
-        self.app.add_handler(CommandHandler("sum", self.summarize_url))
-        self.app.add_handler(CommandHandler("jp", self.translate_jp))
+        self.app.add_handler(CommandHandler("sum", self.summarize_url, filters=filters.Chat(self.whitelist)))
+        self.app.add_handler(CommandHandler("jp", self.translate_jp, filters=filters.Chat(self.whitelist)))
+        self.app.add_handler(CommandHandler("chat_id", self.show_chat_id))
         self.app.run_polling(allowed_updates=Update.ALL_TYPES)
 
     @classmethod
@@ -43,6 +45,16 @@ class Bot:
 
         return cls(token=token, whitelist=[int(chat_id) for chat_id in whitelist.replace(" ", "").split(",")])
 
+    # show chat_id
+    async def show_chat_id(self, update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
+        """
+        Show the chat ID of the current chat.
+        """
+        if not update.message:
+            return
+
+        await update.message.reply_text(f"Chat ID: {update.message.chat_id}")
+
     async def summarize_url(self, update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
         """
         Summarize the URL found in the message text and reply with the summary.
@@ -51,10 +63,6 @@ class Bot:
             return
 
         logger.info("Received message: '{}' from chat ID: {}", update.message.text, update.message.chat_id)
-
-        if update.message.chat_id not in self.whitelist:
-            logger.info("Chat ID {} not in whitelist", update.message.chat_id)
-            return
 
         raw_text = update.message.text
         if update.message.reply_to_message and update.message.reply_to_message.text:
@@ -84,10 +92,6 @@ class Bot:
             return
 
         logger.info("Received message: '{}' from chat ID: {}", update.message.text, update.message.chat_id)
-
-        if update.message.chat_id not in self.whitelist:
-            logger.info("Chat ID {} not in whitelist", update.message.chat_id)
-            return
 
         raw_text = update.message.text
         if update.message.reply_to_message and update.message.reply_to_message.text:
