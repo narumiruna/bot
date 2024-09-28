489be2dd598f615cf41a6b60aa6760ecb369bb87
narumi <toucans-cutouts0f@icloud.com>
2024-09-28 23:40:31 +0800

    add polish command

---
+++ bot/polish.py
@@ -0,0 +1,24 @@
+import functools
+
+from langchain_core.messages import AIMessage
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableSerializable
+
+from .utils import ai_message_repr
+from .utils import get_llm_from_env
+
+PROMPT_TEMPLATE = """Polish the following text: {text}"""
+
+
+@functools.cache
+def get_chain() -> RunnableSerializable:
+    llm = get_llm_from_env()
+    prompt = PromptTemplate.from_template(PROMPT_TEMPLATE)
+    chain = prompt | llm
+    return chain
+
+
+def polish(text: str, lang: str) -> str:
+    chain = get_chain()
+    ai_message: AIMessage = chain.invoke({"text": text, "lang": lang})
+    return ai_message_repr(ai_message)
