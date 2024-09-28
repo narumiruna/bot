9faa8dfda8a0c523e00ccfc7cec2f4c5b7fd24c0
narumi <toucans-cutouts0f@icloud.com>
2024-09-29 01:29:14 +0800

    log message

--- bot/bot.py
+++ bot/bot.py
@@ -8,6 +8,7 @@ from telegram import Update
 from telegram.ext import Application
 from telegram.ext import CommandHandler
 from telegram.ext import ContextTypes
+from telegram.ext import MessageHandler
 from telegram.ext import filters
 
 from .polish import polish
@@ -17,16 +18,7 @@ from .utils import load_document
 from .utils import parse_url
 
 
-def get_full_message_text(update: Update) -> str:
-    """
-    Extract the full text of the message and the text of the reply-to message, if any.
-
-    Args:
-        update (Update): The update object containing the message.
-
-    Returns:
-        str: The combined message text and reply text, if available.
-    """
+def get_message_text(update: Update) -> str:
     message = update.message
     if not message:
         return ""
@@ -34,7 +26,12 @@ def get_full_message_text(update: Update) -> str:
     message_text = message.text or ""
     reply_text = message.reply_to_message.text if message.reply_to_message and message.reply_to_message.text else ""
 
-    return f"{message_text}\n{reply_text}" if reply_text else message_text
+    return f"{reply_text}\n{message_text}" if reply_text else message_text
+
+
+async def log_message_(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.info("Message Update: {}", update)
+    logger.info("Message Context: {}", context)
 
 
 async def show_chat_id_(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
@@ -51,13 +48,10 @@ async def summarize_(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     """
     Summarize the URL found in the message text and reply with the summary.
     """
-
-    logger.info(update)
-
     if not update.message or not update.message.text:
         return
 
-    raw_text = get_full_message_text(update)
+    raw_text = get_message_text(update)
 
     url = parse_url(raw_text)
     if not url:
@@ -81,12 +75,10 @@ async def summarize_(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
 
 def create_translate_callback(lang: str) -> Callable:
     async def translate_(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
-        logger.info(update)
-
         if not update.message or not update.message.text:
             return
 
-        raw_text = get_full_message_text(update)
+        raw_text = get_message_text(update)
 
         translated = translate(raw_text, lang=lang)
         logger.info("Replying to chat ID: {} with: {}", update.message.chat_id, translated)
@@ -97,12 +89,10 @@ def create_translate_callback(lang: str) -> Callable:
 
 
 async def polish_(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
-    logger.info(update)
-
     if not update.message or not update.message.text:
         return
 
-    raw_text = get_full_message_text(update)
+    raw_text = get_message_text(update)
 
     polished = polish(raw_text)
     logger.info("Replying to chat ID: {} with: {}", update.message.chat_id, polished)
@@ -128,4 +118,5 @@ def run_bot() -> None:
     app.add_handler(CommandHandler("en", create_translate_callback("英文"), filters=chat_filter))
     app.add_handler(CommandHandler("polish", polish_, filters=chat_filter))
     app.add_handler(CommandHandler("chat_id", show_chat_id_))
+    app.add_handler(MessageHandler(filters=chat_filter, callback=log_message_))
     app.run_polling(allowed_updates=Update.ALL_TYPES)
