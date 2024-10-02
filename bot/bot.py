18f2cc54492a5c6ee858b183eab6456edc5046c8
narumi <toucans-cutouts0f@icloud.com>
2024-10-02 14:44:22 +0800

    add error handler

--- bot/bot.py
+++ bot/bot.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
+import html
 import json
 import os
+import traceback
 from typing import Callable
 
 from loguru import logger
 from telegram import Update
+from telegram.constants import ParseMode
 from telegram.ext import Application
 from telegram.ext import CommandHandler
 from telegram.ext import ContextTypes
@@ -135,6 +138,35 @@ async def help_(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(help_text)
 
 
+def create_error_handler(developer_chat_id: int) -> Callable:
+    async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Log the error and send a telegram message to notify the developer."""
+        # Log the error before we do anything else, so we can see it even if something breaks.
+        logger.error("Exception while handling an update: {}", context.error)
+
+        # traceback.format_exception returns the usual python message about an exception, but as a
+        # list of strings rather than a single string, so we have to join them together.
+        tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+        tb_string = "".join(tb_list)
+
+        # Build the message with some markup and additional information about what happened.
+        # You might need to add some logic to deal with messages longer than the 4096 character limit.
+        update_str = update.to_dict() if isinstance(update, Update) else str(update)
+        message = (
+            "An exception was raised while handling an update\n"
+            f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+            "</pre>\n\n"
+            f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+            f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
+            f"<pre>{html.escape(tb_string)}</pre>"
+        )
+
+        # Finally, send the message
+        await context.bot.send_message(chat_id=developer_chat_id, text=message, parse_mode=ParseMode.HTML)
+
+    return error_handler
+
+
 def run_bot() -> None:
     token = os.getenv("BOT_TOKEN")
     if not token:
@@ -160,4 +192,9 @@ def run_bot() -> None:
             MessageHandler(filters=chat_filter, callback=log_message_),
         ]
     )
+
+    developer_chat_id = os.getenv("DEVELOPER_CHAT_ID")
+    if developer_chat_id:
+        app.add_error_handler(create_error_handler(developer_chat_id))
+
     app.run_polling(allowed_updates=Update.ALL_TYPES)
