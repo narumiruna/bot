cdb0f12fd64cd0b07efa27321907c4d87789c741
narumi <toucans-cutouts0f@icloud.com>
2024-10-02 16:42:55 +0800

    add whisper support

--- bot/utils.py
+++ bot/utils.py
@@ -1,4 +1,5 @@
 import contextlib
+import functools
 import re
 import subprocess
 import tempfile
@@ -7,11 +8,15 @@ from urllib.parse import urlparse
 from urllib.parse import urlunparse
 
 import httpx
+import numpy as np
+import whisper
+import yt_dlp
 from langchain_community.document_loaders import YoutubeLoader
 from langchain_community.document_loaders.html_bs import BSHTMLLoader
 from langchain_community.document_loaders.pdf import PyPDFLoader
 from langchain_core.documents import Document
 from langchain_core.messages import AIMessage
+from loguru import logger
 
 DEFAULT_HEADERS = {
     "Accept-Language": "zh-TW,zh;q=0.9,ja;q=0.8,en-US;q=0.7,en;q=0.6",
@@ -36,6 +41,8 @@ DOMAIN_REPLACEMENTS = {
     "twitter.com": "api.fxtwitter.com",
     "x.com": "api.fxtwitter.com",
 }
+
+FFMPEG_LOCATION = "/opt/homebrew/bin/ffmpeg"
 
 
 def download_by_httpx(url: str) -> str:
@@ -87,17 +94,33 @@ def docs_to_str(docs: list[Document]) -> str:
     return "\n".join([doc.page_content.strip() for doc in docs])
 
 
-def load_document(url: str) -> str:
-    # https://python.langchain.com/docs/integrations/document_loaders/
+def is_youtube_video_url(url: str) -> bool:
+    return url.startswith("https://www.youtube.com/watch?v=") or url.startswith("https://youtu.be/")
 
+
+def load_youtube_transcripts(url: str) -> str:
     with contextlib.suppress(ValueError):
-        return docs_to_str(
+        transcripts = docs_to_str(
             YoutubeLoader.from_youtube_url(
                 url,
                 add_video_info=True,
                 language=DEFAULT_LANGUAGE_CODES,
             ).load()
         )
+        logger.info("transcripts: {}", transcripts)
+        if transcripts:
+            return transcripts
+
+    # if the video has no transcripts
+    # download the video and transcribe it by whisper
+    return load_transcribe_by_whisper(url)
+
+
+def load_document(url: str) -> str:
+    # https://python.langchain.com/docs/integrations/document_loaders/
+
+    if is_youtube_video_url(url):
+        return load_youtube_transcripts(url)
 
     # replace domain
     url = replace_domain(url)
@@ -139,3 +162,80 @@ def replace_domain(url: str) -> str:
         return urlunparse(fixed_url)
 
     return url
+
+
+def download_by_ytdlp(url: str) -> str:
+    filename = tempfile.mktemp()
+
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }
+        ],
+        "outtmpl": filename,
+        "ffmpeg_location": FFMPEG_LOCATION,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    return filename + ".mp3"
+
+
+def load_audio(file: str, sr: int = 16000):
+    """
+    Open an audio file and read as mono waveform, resampling as necessary
+
+    Parameters
+    ----------
+    file: str
+        The audio file to open
+
+    sr: int
+        The sample rate to resample the audio if necessary
+
+    Returns
+    -------
+    A NumPy array containing the audio waveform, in float32 dtype.
+    """
+
+    # This launches a subprocess to decode audio while down-mixing
+    # and resampling as necessary.  Requires the ffmpeg CLI in PATH.
+    # fmt: off
+    cmd = [
+        FFMPEG_LOCATION,
+        "-nostdin",
+        "-threads", "0",
+        "-i", file,
+        "-f", "s16le",
+        "-ac", "1",
+        "-acodec", "pcm_s16le",
+        "-ar", str(sr),
+        "-"
+    ]
+    # fmt: on
+    try:
+        out = subprocess.run(cmd, capture_output=True, check=True).stdout
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
+
+    return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
+
+
+@functools.cache
+def load_whisper_model() -> whisper.Whisper:
+    return whisper.load_model("tiny")
+
+
+@functools.cache
+def load_transcribe_by_whisper(url: str) -> str:
+    f = download_by_ytdlp(url)
+    audio = load_audio(f)
+    model = load_whisper_model()
+    result = model.transcribe(audio)
+    logger.info("transcribe result: {}", result)
+    return str(result["text"])
