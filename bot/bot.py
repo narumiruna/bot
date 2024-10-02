c8f2d5ff90fc072b13a0bad131a78c38ecc454ee
narumi <toucans-cutouts0f@icloud.com>
2024-10-03 00:17:58 +0800

    send error message to telegraph

--- bot/bot.py
+++ bot/bot.py
@@ -19,6 +19,7 @@ from .polish import polish
 from .summarize import summarize
 from .translate import translate
 from .translate import translate_and_explain
+from .utils import create_page
 from .utils import load_document
 from .utils import parse_url
 from .yahoo_finance import query_tickers
@@ -140,23 +141,24 @@ async def help_callback(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
 async def error_callback(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
     logger.error("Exception while handling an update: {}", context.error)
 
-    # update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
 
-    message = (
+    html_content = (
         "An exception was raised while handling an update\n"
-        # f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}</pre>\n\n"
-        # f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
-        # f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
+        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}</pre>\n\n"
+        f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+        f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
         f"<pre>context.error = {html.escape(str(context.error))}</pre>\n\n"
     )
     if context.error:
         tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
         tb_string = "".join(tb_list)
-        message += f"<pre>Traceback (most recent call last):\n{html.escape(tb_string)}</pre>"
+        html_content += f"<pre>Traceback (most recent call last):\n{html.escape(tb_string)}</pre>"
 
+    page_url = create_page(title="Error", html_content=html_content)
     developer_chat_id = os.getenv("DEVELOPER_CHAT_ID", None)
     if developer_chat_id:
-        await context.bot.send_message(chat_id=developer_chat_id, text=message, parse_mode=ParseMode.HTML)
+        await context.bot.send_message(chat_id=developer_chat_id, text=page_url, parse_mode=ParseMode.MARKDOWN_V2)
 
     # if isinstance(update, Update) and update.message:
     #     await update.message.reply_text(text=message, parse_mode=ParseMode.HTML)
