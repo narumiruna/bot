30bcebd1ced9b44325430801a793d38d7e8e430d
narumi <toucans-cutouts0f@icloud.com>
2024-10-03 14:51:28 +0800

    improve utils.py

--- bot/utils.py
+++ bot/utils.py
@@ -1,4 +1,3 @@
-import contextlib
 import functools
 import os
 import re
@@ -10,19 +9,16 @@ from urllib.parse import urlunparse
 
 import chardet
 import httpx
-import numpy as np
 import telegraph
-import whisper
-import yt_dlp
 from bs4 import BeautifulSoup
-from langchain_community.document_loaders import YoutubeLoader
 from langchain_community.document_loaders.html_bs import BSHTMLLoader
 from langchain_community.document_loaders.pdf import PyPDFLoader
 from langchain_core.documents import Document
 from langchain_core.messages import AIMessage
 from loguru import logger
-from youtube_transcript_api import NoTranscriptFound
-from youtube_transcript_api import TranscriptsDisabled
+
+from .loaders import load_video_transcript
+from .loaders import load_youtube_transcript
 
 DEFAULT_HEADERS = {
     "Accept-Language": "zh-TW,zh;q=0.9,ja;q=0.8,en-US;q=0.7,en;q=0.6",
@@ -41,10 +37,6 @@ DOMAIN_REPLACEMENTS = {
     "twitter.com": "api.fxtwitter.com",
     "x.com": "api.fxtwitter.com",
 }
-
-
-def get_ffmpeg_path_from_env() -> str:
-    return os.getenv("FFMPEG_PATH", "/opt/homebrew/bin/ffmpeg")
 
 
 def is_pdf(url: str) -> bool:
@@ -138,29 +130,18 @@ def is_youtube_url(url: str) -> bool:
     )
 
 
-def load_youtube_transcripts(url: str) -> str:
-    with contextlib.suppress(ValueError, NoTranscriptFound, TranscriptsDisabled):
-        transcripts = docs_to_str(
-            YoutubeLoader.from_youtube_url(
-                url,
-                add_video_info=False,
-                language=DEFAULT_LANGUAGE_CODES,
-            ).load()
-        )
-        logger.info("transcripts: {}", transcripts)
-        if transcripts:
-            return transcripts
-
-    # if the video has no transcripts
-    # download the video and transcribe it by whisper
-    return load_transcribe_by_whisper(url)
-
-
 def load_document(url: str) -> str:
     # https://python.langchain.com/docs/integrations/document_loaders/
 
     if is_youtube_url(url):
-        return load_youtube_transcripts(url)
+        transcript = load_youtube_transcript(url)
+        if transcript:
+            return transcript
+        logger.info("No transcript found for YouTube video: {}", url)
+
+        # if the video has no transcripts
+        # download the video and transcribe it by whisper
+        return load_video_transcript(url)
 
     # replace domain
     url = replace_domain(url)
@@ -198,85 +179,6 @@ def replace_domain(url: str) -> str:
         return urlunparse(fixed_url)
 
     return url
-
-
-def ytdlp_download(url: str) -> str:
-    ffmpeg_path = get_ffmpeg_path_from_env()
-
-    filename = tempfile.mktemp()
-
-    ydl_opts = {
-        "format": "bestaudio/best",
-        "postprocessors": [
-            {
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
-                "preferredquality": "192",
-            }
-        ],
-        "outtmpl": filename,
-        "ffmpeg_location": ffmpeg_path,
-    }
-
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-
-    return filename + ".mp3"
-
-
-def load_audio(file: str, sr: int = 16000):
-    """
-    Open an audio file and read as mono waveform, resampling as necessary
-
-    Parameters
-    ----------
-    file: str
-        The audio file to open
-
-    sr: int
-        The sample rate to resample the audio if necessary
-
-    Returns
-    -------
-    A NumPy array containing the audio waveform, in float32 dtype.
-    """
-    ffmpeg_path = get_ffmpeg_path_from_env()
-
-    # This launches a subprocess to decode audio while down-mixing
-    # and resampling as necessary.  Requires the ffmpeg CLI in PATH.
-    # fmt: off
-    cmd = [
-        ffmpeg_path,
-        "-nostdin",
-        "-threads", "0",
-        "-i", file,
-        "-f", "s16le",
-        "-ac", "1",
-        "-acodec", "pcm_s16le",
-        "-ar", str(sr),
-        "-"
-    ]
-    # fmt: on
-    try:
-        out = subprocess.run(cmd, capture_output=True, check=True).stdout
-    except subprocess.CalledProcessError as e:
-        raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
-
-    return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
-
-
-@functools.cache
-def load_whisper_model() -> whisper.Whisper:
-    return whisper.load_model("tiny")
-
-
-@functools.cache
-def load_transcribe_by_whisper(url: str) -> str:
-    f = ytdlp_download(url)
-    audio = load_audio(f)
-    model = load_whisper_model()
-    result = model.transcribe(audio)
-    return str(result["text"])
 
 
 @functools.cache
