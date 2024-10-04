cb470e427f6baeb092b30814908a62537c030a6b
narumi <toucans-cutouts0f@icloud.com>
2024-10-04 14:37:35 +0800

    add log

--- bot/loaders/pdf.py
+++ bot/loaders/pdf.py
@@ -2,18 +2,21 @@ import tempfile
 
 import httpx
 from langchain_community.document_loaders.pdf import PyPDFLoader
+from loguru import logger
 
 from .utils import docs_to_str
 
-DEFAULT_HEADERS = {
-    "Accept-Language": "zh-TW,zh;q=0.9,ja;q=0.8,en-US;q=0.7,en;q=0.6",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",  # noqa
-    "Cookie": "over18=1",  # ptt
-}
-
 
 def load_pdf(url: str) -> str:
-    resp = httpx.get(url=url, headers=DEFAULT_HEADERS, follow_redirects=True)
+    logger.info("Loading PDF: {}", url)
+
+    headers = {
+        "Accept-Language": "zh-TW,zh;q=0.9,ja;q=0.8,en-US;q=0.7,en;q=0.6",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",  # noqa
+        "Cookie": "over18=1",  # ptt
+    }
+
+    resp = httpx.get(url=url, headers=headers, follow_redirects=True)
     resp.raise_for_status()
 
     suffix = ".pdf" if resp.headers.get("content-type") == "application/pdf" else None
