85cd85152be1a53a0b01384d1664a0faba082eee
narumi <toucans-cutouts0f@icloud.com>
2024-10-19 02:40:19 +0800

    fix error

--- bot/cli.py
+++ bot/cli.py
@@ -2,10 +2,8 @@ from dotenv import find_dotenv
 from dotenv import load_dotenv
 
 from .bot import run_bot
-from .llm import set_sqlite_llm_cache
 
 
 def main():
     load_dotenv(find_dotenv(raise_error_if_not_found=True, usecwd=True))
-    set_sqlite_llm_cache()
     run_bot()
