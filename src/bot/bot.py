82d77bf12b20e212e34fb7558e55745630a1402d
narumi <toucans-cutouts0f@icloud.com>
2024-11-01 23:37:16 +0800

    move log cb

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -2,52 +2,13 @@ from __future__ import annotations
 
 import os
 
-from loguru import logger
 from telegram import Update
 from telegram.ext import Application
 from telegram.ext import CommandHandler
-from telegram.ext import ContextTypes
 from telegram.ext import MessageHandler
 from telegram.ext import filters
 
 from . import cb
-
-# from ..qdrant import add_to_qdrant
-from .qdrant import query_qdrant
-
-
-async def log_update(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
-    logger.info("Message Update: {}", update)
-
-    if not update.message:
-        return
-
-    if not update.message.text:
-        return
-
-    if "/query" in update.message.text:
-        return
-
-    # logger.info("Upserting to Qdrant: {}", update.message.text)
-    # add_to_qdrant(
-    #     update.message.text,
-    #     chat_id=update.message.chat.id,
-    #     message_id=update.message.message_id,
-    # )
-
-
-async def query(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if not update.message:
-        return
-
-    message_text = cb.get_message_text(update)
-    if not message_text:
-        return
-
-    text = query_qdrant(message_text, chat_id=update.message.chat.id)
-    if not text:
-        text = "No results found"
-    await update.message.reply_text(text)
 
 
 def run_bot() -> None:
@@ -74,13 +35,11 @@ def run_bot() -> None:
             CommandHandler("twse", cb.query_twse_ticker, filters=chat_filter),
             CommandHandler("yt", cb.search_youtube, filters=chat_filter),
             CommandHandler("g", cb.search_google, filters=chat_filter),
-            # CommandHandler("query", query, filters=chat_filter),
-            # CommandHandler("prompt", generate_prompt, filters=chat_filter),
             CommandHandler("echo", cb.echo),
             MessageHandler(filters=chat_filter, callback=cb.summarize_document),
         ]
     )
-    app.add_handler(MessageHandler(filters=chat_filter, callback=log_update), group=1)
+    app.add_handler(MessageHandler(filters=chat_filter, callback=cb.log), group=1)
 
     app.add_error_handler(cb.error_callback)
     app.run_polling(allowed_updates=Update.ALL_TYPES)
