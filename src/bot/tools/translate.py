50c6f7d0f9440253c26954fbf711e21e05900629
なるみ <4680567+narumiruna@users.noreply.github.com>
2024-11-22 02:20:34 +0800

    Update translate.py

--- src/bot/tools/translate.py
+++ src/bot/tools/translate.py
@@ -2,24 +2,18 @@ from lazyopenai import generate
 
 
 def translate(text: str, lang: str) -> str:
-    user_prompt = f"""
-    Text:
-    {text}
-    """.strip()
+    user_prompt = f'"""{text}"""'
 
     system_prompt = f"""
-    Translate the text into {lang}.
+    Translate the text delimited by triple quotes into {lang}.
     """.strip()
     return generate(user_prompt, system=system_prompt)
 
 
 def translate_and_explain(text: str, lang: str) -> str:
-    user_prompt = f"""
-    Text:
-    {text}
-    """.strip()
+    user_prompt = f'"""{text}"""'
 
     system_prompt = f"""
-    Translate the text into {lang}, and provide a concise explanation of grammar and usage, along with example sentences to enhance understanding."
+    Translate the text delimited by triple quotes into {lang}, and provide a concise explanation of grammar and usage, along with example sentences to enhance understanding."
     """.strip()  # noqa
     return generate(user_prompt, system=system_prompt)
