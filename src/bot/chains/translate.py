a1f846850ab9bf2093aa89624eea6ee232d463dd
narumi <toucans-cutouts0f@icloud.com>
2024-12-02 14:21:39 +0800

    update prompt

--- src/bot/chains/translate.py
+++ src/bot/chains/translate.py
@@ -14,6 +14,6 @@ def translate_and_explain(text: str, lang: str) -> str:
     user_prompt = f'"""{text}"""'
 
     system_prompt = f"""
-    Translate the text delimited by triple quotation marks into {lang}, and provide a concise explanation of grammar and usage, along with example sentences to enhance understanding."
+    Translate the text delimited by triple quotation marks into {lang}, and provide a concise explanation of grammar and usage in {lang}, along with example sentences to enhance understanding."
     """.strip()  # noqa
     return generate(user_prompt, system=system_prompt)
