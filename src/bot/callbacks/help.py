d32d44626b877528812e94ee9aeaeff33dd665d9
なるみ <4680567+narumiruna@users.noreply.github.com>
2024-12-05 19:01:52 +0800

    Update help.py

--- src/bot/callbacks/help.py
+++ src/bot/callbacks/help.py
@@ -9,7 +9,7 @@ async def help(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
         return
 
     help_text = (
-        "code: https://github.com/narumiruna/bot/tree/main/bot\n"
+        "code: https://github.com/narumiruna/bot\n"
         "/help - Show this help message\n"
         "/sum - Summarize a document or URL content\n"
         "/jp - Translate text to Japanese\n"
