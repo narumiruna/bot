65aede99e0dcf31dbfe0528c01f4ca1b642f7d9d
narumi <toucans-cutouts0f@icloud.com>
2024-12-17 22:26:05 +0800

    add replace_domain

--- src/bot/loaders/v2/url.py
+++ src/bot/loaders/v2/url.py
@@ -1,3 +1,6 @@
+from urllib.parse import urlparse
+from urllib.parse import urlunparse
+
 from loguru import logger
 
 from .cloudscraper import CloudscraperLoader
@@ -7,6 +10,27 @@ from .loader import LoaderError
 from .pdf import PDFLoader
 from .singlefile import SinglefileLoader
 from .youtube import YoutubeTranscriptLoader
+
+REPLACEMENTS = {
+    "api.fxtwitter.com": [
+        "twitter.com",
+        "x.com",
+        "fxtwitter.com",
+        "vxtwitter.com",
+        "fixvx.com",
+        "twittpr.com",
+        "fixupx.com",
+    ]
+}
+
+
+def replace_domain(url: str) -> str:
+    parsed = urlparse(url)
+    for target, source in REPLACEMENTS.items():
+        if parsed.netloc in source:
+            fixed_url = parsed._replace(netloc=target)
+            return urlunparse(fixed_url)
+    return url
 
 
 class URLLoader(Loader):
@@ -23,6 +47,8 @@ class URLLoader(Loader):
         self.loaders.append(loader)
 
     def load(self, url: str) -> str:
+        url = replace_domain(url)
+
         for loader in self.loaders:
             try:
                 return loader.load(url)
