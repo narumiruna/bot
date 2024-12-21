82e7cb0682c2f77aa5a30f9548d30bee7a8c5475
narumi <toucans-cutouts0f@icloud.com>
2024-12-22 03:40:56 +0800

    delete load_video_transcript

--- src/bot/loaders/__init__.py
+++ src/bot/loaders/__init__.py
@@ -2,5 +2,4 @@ from .html import load_html_file
 from .pdf import load_pdf
 from .pdf import load_pdf_file
 from .v2 import URLLoader
-from .video_transcript import load_video_transcript
 from .youtube_transcript import load_youtube_transcript
