81bbb88e22019ced86f9490c0ecf7d125d6f2407
narumi <toucans-cutouts0f@icloud.com>
2024-12-21 23:29:48 +0800

    disable sum command

--- src/bot/callbacks/summarize.py
+++ src/bot/callbacks/summarize.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import os
 
-from loguru import logger
 from telegram import Update
 from telegram.constants import ParseMode
 from telegram.ext import ContextTypes
@@ -10,8 +9,6 @@ from telegram.ext import ContextTypes
 from .. import chains
 from ..loaders import load_html_file
 from ..loaders import load_pdf_file
-from ..loaders import load_url
-from ..utils import parse_url
 from .utils import get_message_text
 
 
@@ -46,33 +43,5 @@ async def summarize(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     message_text = get_message_text(update)
     if not message_text:
         return
-    logger.info("message_text: {}", message_text)
 
-    url = parse_url(message_text)
-    if not url:
-        # if no URL is found, summarize the message text
-        # TODO: simplify this logic
-        result = chains.summarize(message_text)
-        logger.info("Summarized text: {}", result)
-        await update.message.reply_text(result, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
-        return
-    logger.info("Parsed URL: {}", url)
-
-    # remove the URL from the message text
-    question = None
-    if context.args:
-        question = " ".join(context.args).replace(url, "").strip()
-
-    text = await load_url(url)
-    if not text:
-        await update.message.reply_text(f"Unable to load content from: {url}")
-        return
-    logger.info("Text length: {}", len(text))
-
-    result = chains.summarize(text)
-    if question:
-        result += f"\n\n{chains.answer_question(text, question)}"
-
-    logger.info("Summarized text: {}", result)
-
-    await update.message.reply_text(result, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+    await update.message.reply_text("改用 /s <url>")
