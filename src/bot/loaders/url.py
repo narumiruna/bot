62e9d02457190b763f53613c047d3b81324c38da
narumi <toucans-cutouts0f@icloud.com>
2024-12-22 03:46:56 +0800

    delete is_pdf_url

--- src/bot/loaders/url.py
+++ src/bot/loaders/url.py
@@ -1,19 +1,6 @@
 from urllib.parse import urlparse
 from urllib.parse import urlunparse
 
-import httpx
-
-
-def is_pdf_url(url: str) -> bool:
-    headers = {
-        "Accept-Language": "zh-TW,zh;q=0.9,ja;q=0.8,en-US;q=0.7,en;q=0.6",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",  # noqa
-    }
-
-    resp = httpx.head(url=url, headers=headers, follow_redirects=True)
-    resp.raise_for_status()
-    return resp.headers.get("content-type") == "application/pdf"
-
 
 def is_youtube_url(url: str) -> bool:
     return (
