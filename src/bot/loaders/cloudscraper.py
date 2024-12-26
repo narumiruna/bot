2e3c5238bce1508fa7740aee3e6d9f9b67daf472
narumi <toucans-cutouts0f@icloud.com>
2024-12-27 01:48:24 +0800

    add browser-headless arg

--- src/bot/loaders/cloudscraper.py
+++ src/bot/loaders/cloudscraper.py
@@ -7,6 +7,6 @@ from .utils import html_to_markdown
 class CloudscraperLoader(Loader):
     def load(self, url: str) -> str:
         client = cloudscraper.create_scraper()
-        response = client.get(url)
+        response = client.get(url, allow_redirects=True)
         response.raise_for_status()
         return html_to_markdown(response.text)
