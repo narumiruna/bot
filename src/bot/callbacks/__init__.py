f74cc0d2e729a8646a3803931b4afd5f4f5b489c
narumi <toucans-cutouts0f@icloud.com>
2025-01-05 16:21:11 +0800

    ♻️ refactor(callbacks): remove unused query_twse_ticker function

--- src/bot/callbacks/__init__.py
+++ src/bot/callbacks/__init__.py
@@ -15,6 +15,5 @@ from .summary import summarize_document
 from .ticker import query_ticker
 from .translate import create_translate_callback
 from .trip import handle_trip
-from .twse import query_twse_ticker
 from .utils import get_message_text
 from .youtube_search import search_youtube
