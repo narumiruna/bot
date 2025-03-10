8393d59a9b09fa09a9efc947f72c37a933815299
narumi <toucans-cutouts0f@icloud.com>
2025-03-10 22:00:03 +0800

    ✨ feat(chains): enhance generate function with additional parameters

--- src/bot/chains/utils.py
+++ src/bot/chains/utils.py
@@ -2,10 +2,17 @@ import asyncio
 import concurrent.futures
 
 import lazyopenai
+from lazyopenai.chat import BaseTool
+from lazyopenai.chat import ResponseFormatT
 
 
-async def generate(*args, **kwargs):
+async def generate(
+    messages: str | list[str],
+    system: str | None = None,
+    response_format: type[ResponseFormatT] | None = None,
+    tools: list[type[BaseTool]] | None = None,
+):
     loop = asyncio.get_event_loop()
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        result = await loop.run_in_executor(executor, lazyopenai.generate, *args, **kwargs)
+        result = await loop.run_in_executor(executor, lazyopenai.generate, messages, system, response_format, tools)
         return result
