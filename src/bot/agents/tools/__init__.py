a7751d163ca45dba174f014ad524bb8d56b37d3d
narumi <toucans-cutouts0f@icloud.com>
2025-03-13 03:13:34 +0800

    ✨ feat(mortgage): move loan details computation functionality

--- src/bot/agents/tools/__init__.py
+++ src/bot/agents/tools/__init__.py
@@ -2,5 +2,6 @@ from .datetime import get_current_time
 from .duckduckgo import extract_content
 from .duckduckgo import web_search
 from .monster_hunter_weapon import draw_monster_hunter_weapon
+from .mortgage import compute_loan_details
 from .tarot import draw_tarot_card
 from .yahoo_finance import query_ticker_from_yahoo_finance
--- src/bot/tools/__init__.py
+++ src/bot/tools/__init__.py
@@ -1,3 +1,2 @@
-from .mortgage import LoanTool
 from .weblio import Weblio
 from .yahoo_finance import query_tickers
