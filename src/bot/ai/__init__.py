9b8b6cf2e23ae8ca9d83074e5342e58870c11820
narumi <toucans-cutouts0f@icloud.com>
2025-04-04 01:47:48 +0800

    delete agents

--- src/bot/ai/__init__.py
+++ src/bot/ai/__init__.py
@@ -1,5 +1,3 @@
-from .default import get_default_agent
-from .fortune_teller import get_fortune_teller_agent
 from .model import get_openai_model
 from .model import get_openai_model_settings
 from .service import AgentService
