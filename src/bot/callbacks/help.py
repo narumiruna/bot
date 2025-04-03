e4be23229a8ab67d5c74d8614a9a9bdc8d52a5fd
narumi <toucans-cutouts0f@icloud.com>
2025-04-03 15:19:20 +0800

    update help

--- src/bot/callbacks/help.py
+++ src/bot/callbacks/help.py
@@ -8,21 +8,24 @@ async def handle_help(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     if not update.message:
         return
 
-    help_text = (
-        "code: https://github.com/narumiruna/bot\n"
-        "/help - Show this help message\n"
-        "/a - An agent that can assist with various tasks"
-        "/s - Summarize a document or URL content\n"
-        "/jp - Translate text to Japanese\n"
-        "/tc - Translate text to Traditional Chinese\n"
-        "/en - Translate text to English\n"
-        "/polish - Polish and improve text\n"
-        "/echo - Echo the message\n"
-        "/yt - Search YouTube\n"
-        "/recipe - Generate a recipe\n"
-        "/ljp - Learn Japanese\n"
-        "/t - Query ticker from Yahoo Finance and Taiwan stock exchange\n"
-        "/f - Format and normalize the document in 台灣話\n"
+    await update.message.reply_text(
+        "\n".join(
+            [
+                "code: https://github.com/narumiruna/bot",
+                "/help - Show this help message",
+                "/a - An agent that can assist with various tasks",
+                "/s - Summarize a document or URL content",
+                "/jp - Translate text to Japanese",
+                "/tc - Translate text to Traditional Chinese",
+                "/en - Translate text to English",
+                "/polish - Polish and improve text",
+                "/echo - Echo the message",
+                "/yt - Search YouTube",
+                "/recipe - Generate a recipe",
+                "/ljp - Learn Japanese",
+                "/t - Query ticker from Yahoo Finance and Taiwan stock exchange",
+                "/f - Format and normalize the document in 台灣話",
+            ]
+        ),
+        disable_web_page_preview=True,
     )
-
-    await update.message.reply_text(help_text, disable_web_page_preview=True)
