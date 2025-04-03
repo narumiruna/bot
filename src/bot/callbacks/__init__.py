e258749bb970206cb03d7624f8052fa368e3ad57
narumi <toucans-cutouts0f@icloud.com>
2025-04-03 18:35:24 +0800

    delete trip callback

--- src/bot/callbacks/__init__.py
+++ src/bot/callbacks/__init__.py
@@ -8,6 +8,5 @@ from .log import log_message_update
 from .summary import summarize
 from .ticker import query_ticker
 from .translate import create_translate_callback
-from .trip import handle_trip
 from .utils import get_message_text
 from .youtube_search import search_youtube
